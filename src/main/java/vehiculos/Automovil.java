bebc491f4066906c6532c20e48fb161730c6482e
LeManuelo <lemanuelo04s@gmail.com>
2023-03-26 16:45:59 -0500

    Push_1

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,30 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo {
+
+    private int puestos;
+    private static int cantidadAutomoviles;
+
+    public Automovil(String placa, String nombre, int precio, int peso, Fabricante facbricante, int puestos, ) {
+        super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+        this.puestos = puestos;
+        cantidadAutomoviles++;
+    }
+
+    public int getPuestos() {
+        return puestos;
+    }
+
+    public voud setPuestos(int puestos) {
+        this.puestos = puestos;
+    }
+
+    public static int getCantidadAutos(int cantidadAutomoviles){
+        return cantidadAutomoviles;
+    }
+
+    public static void setCantidadAutos(int cantidadAutomoviles){
+        Automovil.cantidadAutomoviles = cantidadAutomoviles;
+    }
+
+}
